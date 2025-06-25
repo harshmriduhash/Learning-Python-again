829dc766006dbcc111881491592092c5b423e01d
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-06-25 23:35:26 +0530

    Notes added

--- 01_Basics/Hello_chai.py
+++ 01_Basics/Hello_chai.py
@@ -4,6 +4,6 @@ def chai(n):
     print(n)
 chai("lemon tea")
 
-chai_one = "lemon tea"
-chai_two = "ginger tea"
+chai_one = "lemon tea" 
+chai_two = "ginger tea" #to add a comment press ctrl+/#
 chai_three = "masala tea" #variable are also called attributes, You have to reload when using terminal by running command of "fromimportlib import reload"#
